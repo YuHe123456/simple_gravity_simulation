de2284b70ef2fb30fd6c3de923b47a7bf796e72b
YuHe123456 <yu.he31415@gmail.com>
2026-08-12 00:07:50 +0100

    Added "tick" and "draw_ball" subprograms.

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ SCREEN_HEIGHT = 720
 
 
 clock = pg.time.Clock()
-clock.tick(60)
 
-dt = clock.tick(60) / 1000
+clock.tick(1)
+dt = clock.tick(1) / 1000
 
 class Ball:
     def __init__(self, x, y, x_v, y_v, colour, size):
@@ -18,10 +18,12 @@ class Ball:
         
         self.colour = colour
         self.size = size
-
+    
     def tick(self):
         self.pos = self.pos + (self.vel * dt)
 
+    def draw_ball(self, screen):
+        pg.draw.circle(screen, self.colour, self.pos, self.size)
 
 screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pg.display.set_caption("Simulation")
@@ -30,15 +32,20 @@ pg.draw.circle(screen, (255,0,0), (640,360), 5)
 
 running = True
 
+ball1 = Ball(720,360, 5,0,(255,0,0), 5)
+
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
 
-
     # update code
 
+    screen.fill((255,255,255))
+
+    ball1.tick()
+    ball1.draw_ball(screen)
 
     
-
+    
     pg.display.flip()
