d73462929e62ade2aba38db671e5e3f67dabe668
YuHe123456 <yu.he31415@gmail.com>
2026-08-11 23:45:14 +0100

    Created "Ball" object to store a single object's pos and vel

--- main.py
+++ main.py
@@ -2,8 +2,31 @@ import pygame as pg
 
 pg.init()
 
-screen = pg.display.set_mode((1280,720))
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+
+clock = pg.time.Clock()
+clock.tick(60)
+
+dt = clock.tick(60) / 1000
+
+class Ball:
+    def __init__(self, x, y, x_v, y_v, colour, size):
+        self.pos = pg.math.Vector2(x,y)
+        self.vel = pg.math.Vector2(x_v, y_v)
+        
+        self.colour = colour
+        self.size = size
+
+    def tick(self):
+        self.pos = self.pos + (self.vel * dt)
+
+
+screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pg.display.set_caption("Simulation")
+
+pg.draw.circle(screen, (255,0,0), (640,360), 5)
 
 running = True
 
@@ -12,6 +35,10 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
+
     # update code
+
+
+    
 
     pg.display.flip()
