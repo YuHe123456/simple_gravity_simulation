8afc0f27854ab3a8104b7fcfbfd070b36d78a6b6
YuHe123456 <yu.he31415@gmail.com>
2026-08-16 22:40:16 +0100

    Now uses iteration through a list copy instead of modifying a list during iteration.

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class Ball:
 
         self.create_trail_ball()
 
-        for trail_ball in self.trail_list:
+        for trail_ball in self.trail_list[:]:
             if trail_ball.draw_trail_ball(screen) is False:
                 self.trail_list.remove(trail_ball)
 
@@ -49,13 +49,14 @@ class Ball:
             self.trail_list.append(trail_ball)
 
     def find_acc(self, obj_list):
+
+        self.acc = pg.Vector2(0,0)
+
         for obj in obj_list:
+
             if obj is not self:
 
-                self.acc = pg.Vector2(0,0)
-
                 distance = self.pos.distance_to(obj.pos)
-                
 
                 # if distance is less than size of target obj
                 #   mass = ((actual distance / size) ** (1/2)) * mass
@@ -66,12 +67,10 @@ class Ball:
                 acc_unit_vector = (obj.pos - self.pos).normalize()
                 
                 if distance >= obj.size:
-                    effective_mass = obj.mass
-                    self.acc += acc_unit_vector * (effective_mass / distance**2)
+                    self.acc += acc_unit_vector * (obj.mass / distance**2)
 
                 elif distance < obj.size:
-                    effective_mass = obj.mass
-                    self.acc += acc_unit_vector * (effective_mass / obj.size**3) * distance
+                    self.acc += acc_unit_vector * (obj.mass / obj.size**3) * distance
 
                 # acc_vector = (effective_mass * acc_unit_vector) / (distance**2)
 
@@ -84,7 +83,7 @@ class TrailBall:
         self.pos = pos
         self.size = size
         self.colour = self.trail_colour
-        self.countdown = self.countdown
+        self.countdown = TrailBall.countdown
 
     def draw_trail_ball(self, screen):
         pg.draw.circle(screen, self.colour, self.pos, self.size)
@@ -97,15 +96,15 @@ class TrailBall:
 screen = pg.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pg.display.set_caption("Simulation")
 
-pg.draw.circle(screen, (255,0,0), (640,360), 5)
-
 running = True
 
-ball1 = Ball(640, 180, -100, 0, (255,0,0), 5, trail=True)
-ball2 = Ball(640, 360, 0, 0, (0,255,0), 100)
-ball3 = Ball(320, 360, 0, 0, (255,255,0), 100)
+ball1 = Ball(480, 480, 5, 0, (255,0,0), 20, trail=True)
+ball2 = Ball(640, 360, 0, -10, (0,255,0), 20, trail=True)
+ball3 = Ball(320, 360, 10, 0, (255,255,0), 20, trail=True)
 
 objects = [ball1, ball2, ball3][::-1]
+
+
 
 while running:
     for event in pg.event.get():
